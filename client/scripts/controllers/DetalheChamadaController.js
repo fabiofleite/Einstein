b60977eb89db044f8c0620703b09ef4f2a34e4f3
deciomoritz <deciomoritz1@gmail.com>
2016-05-12 20:45:12 -0300

    backend detalhe chamada ok, collections criadas, quasar integrado

--- client/scripts/controllers/DetalheChamadaController.js
+++ client/scripts/controllers/DetalheChamadaController.js
@@ -1,31 +1,46 @@
 angular.module("Estrutura-Inicial")
     .controller('DetalheChamadaController', DetalheChamadaController);
 
-function DetalheChamadaController($location, $scope) {
+function DetalheChamadaController($location, $scope, $ionicScrollDelegate, $stateParams, $http) {
     var vm = this;
 
-    vm.goToFirstPage = function() {
-        $location.path('/first-page');
-    }
+	let alunoId = parseInt($stateParams.alunoId.slice(1));
+	let aluno = Alunos.findOne({matricula: alunoId});
 
-    $scope.aluno = {
-        nome: "Joaozinho",
-        idade: 20,
-        matricula: 55555,
-        faltas : {
-            total: 10,
-            fisica: 2,
-            quimica: 3,
-            biologia: 5
-    },
-        foto: 'https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png'
-    };
+	let query = 'select distinct a.nome,t.disciplina, count(c) as numFaltas from chamadas as c join turmas as t on c.turmaId = t.turmaId join alunos as a on a.matricula = c.alunoId group by a.nome,t.disciplina having c.presente=0 and a.matricula='+alunoId;
+	let databaseName = 'meteor';
+	let url = "http://localhost:9090/query/fs/"+databaseName+"/?q="+query;
+
+	Tracker.autorun(function() {
+		var infoAluno;
+		HTTP.get(url, {timeout:30000}, function(error, response) {
+			if(response){
+				let results = response.content.split("\n");
+				results.splice(-1,1);
+				let infoAluno = _.map(results, function(r) {
+					return JSON.parse(r);
+				});
+
+				$scope.aluno = {
+			        nome: aluno.nome,
+			        idade: aluno.idade,
+			        matricula: aluno.matricula,
+			        foto: aluno.foto
+			    };
+
+				$scope.faltas = infoAluno;
+				console.log($scope.faltas);
+				if (!$scope.$$phase){$scope.$apply();}
+			}
+		});
+	});
 
     $scope.toggleGroup = function() {
         if ($scope.isGroupShown()) {
             $scope.shownGroup = null;
         } else {
             $scope.shownGroup = true;
+			$ionicScrollDelegate.scrollBottom();
         }
     };
     $scope.isGroupShown = function() {
